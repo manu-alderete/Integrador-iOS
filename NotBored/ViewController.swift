de8015d5f6b3804246b72d1673aee376f45750cc
manu-alderete <manuel.alderete@mercadolibre.com>
2021-09-21 11:28:23 -0300

    Initial Commit

---
+++ NotBored/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotBored
+//
+//  Created by Manuel Tomas Alderete Jabif on 21/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
