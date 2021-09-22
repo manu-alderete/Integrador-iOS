478fec1c749e02098773695ad735933d4af8de1a
agfigueroa <agustin.figueroa@mercadolibre.com>
2021-09-22 11:51:33 -0300

    SuggestionViewController added

---
+++ NotBored/Application/Initial/Home/Sugestion/SuggestionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SuggestionViewController.swift
+//  NotBored
+//
+//  Created by Agustin Figueroa on 21/09/2021.
+//
+
+import UIKit
+
+class SuggestionViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        hidesNavigationBar = true
+    }
+
+}
